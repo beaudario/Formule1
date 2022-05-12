e3c89c3d1cc5a9eb1e6ed63c2070c1d030815111
beaudario <dario.douma@upcmail.nl>
2022-05-12 12:09:33 +0200

    added old files

---
+++ Formule1WebApplication/Controllers/SeasonController.cs
@@ -0,0 +1,24 @@
+﻿using Formule1Library.Data;
+using Formule1Library.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formule1WebApplication.Controllers;
+
+public class SeasonController : Controller
+{
+    private readonly Formule1DbContext _db;
+
+    public SeasonController(Formule1DbContext db)
+    {
+        _db = db;
+    }
+
+    [Route("Seizoen/{id:int}")]
+    public async Task<IActionResult> Index(int id)
+    {
+        return View(await _db.Results
+            .Where(r => r.Year == id)
+            .ToListAsync());
+    }
+}
